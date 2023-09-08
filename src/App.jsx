1130f34c8d6c4a95107b825fb3d655221787f666
Federico <cuervog992@gmail.com>
2023-09-07 21:14:51 -0300

    creacion proyecto + header + main

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Calculadora de Potencia</h1>
+      </header>
+      <main>
+        <h1>¿Que desea calcular?</h1>
+        <button>Planas</button>
+      </main>
+    </>
+  )
+}
+
+export default App
